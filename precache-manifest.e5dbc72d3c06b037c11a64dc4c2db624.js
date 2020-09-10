aaf7e4b02a9f7867a458aa3244de369820462924
Jihun Lee <zeroion83@gmail.com>
2020-09-10 23:13:42 +0900

    Updates

---
+++ precache-manifest.e5dbc72d3c06b037c11a64dc4c2db624.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6445336778feb52ba9e73fedf1bdb8b",
+    "url": "/drop-rate/index.html"
+  },
+  {
+    "revision": "ed06b8f8c4a0cc07a6d7",
+    "url": "/drop-rate/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "51c9f48304f3365f1227",
+    "url": "/drop-rate/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ed06b8f8c4a0cc07a6d7",
+    "url": "/drop-rate/static/js/2.938ff1cb.chunk.js"
+  },
+  {
+    "revision": "e53763128d977c2aa839cd529c578e9f",
+    "url": "/drop-rate/static/js/2.938ff1cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51c9f48304f3365f1227",
+    "url": "/drop-rate/static/js/main.e3e31492.chunk.js"
+  },
+  {
+    "revision": "1dd5ebcf40ae98513896",
+    "url": "/drop-rate/static/js/runtime-main.599bbdb6.js"
+  }
+]);
